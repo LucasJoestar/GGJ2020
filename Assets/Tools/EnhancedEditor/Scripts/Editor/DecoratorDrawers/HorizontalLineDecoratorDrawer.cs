425d6c119d9ed3f41da199af412acd950369b481
Lucas <lucasjoestar@outlook.fr>
2020-01-31 21:47:15 +0100

    Tools

---
+++ Assets/Tools/EnhancedEditor/Scripts/Editor/DecoratorDrawers/HorizontalLineDecoratorDrawer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace EnhancedEditor.Editor
+{
+    [CustomPropertyDrawer(typeof(HorizontalLineAttribute))]
+    public class HorizontalLineDecoratorDrawer : DecoratorDrawer
+    {
+        #region Methods
+        /***************************
+         *******   METHODS   *******
+         ***************************/
+
+        // Specify how tall the GUI for this decorator is in pixels
+        public override float GetHeight()
+        {
+            return ((HorizontalLineAttribute)attribute).Height + EditorGUIUtility.singleLineHeight;
+        }
+
+        // Make your own GUI for the decorator
+        public override void OnGUI(Rect _position)
+        {
+            HorizontalLineAttribute _attribute = (HorizontalLineAttribute)attribute;
+
+            // Set position at the middle of the area and line height
+            _position.y += (EditorGUIUtility.singleLineHeight / 2f) - 1;
+            _position.height = _attribute.Height;
+
+            // Specify line width if needed
+            if ((_attribute.Width > 0) && (_attribute.Width < _position.width))
+            {
+                _position.x += (_position.width - _attribute.Width) / 2f;
+                _position.width = _attribute.Width;
+            }
+
+            // Draw rect at position of specified color
+            EditorGUI.DrawRect(_position, _attribute.Color.GetColor());
+        }
+        #endregion
+    }
+}
---
+++ Assets/Tools/EnhancedEditor/Scripts/Example/Example.cs
@@ -0,0 +1,44 @@
+﻿using EnhancedEditor;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    /**************************
+     *******   FIELDS   *******
+     *************************/
+
+    [HorizontalLine(1, order = 0)]
+    [Section("NEW SUPER SECTION MUCH TOO MUCH TOO MUCH TOO LONG", 50, 0, order = 1)]
+    [Space(order = 2)]
+
+    [SerializeField]
+    private string myString = "This is a string";
+
+    [HorizontalLine(2, SuperColor.Red)]
+
+    [SerializeField]
+    private string myString2 = "This is another string";
+
+    [HorizontalLine(2, SuperColor.Sapphire)]
+
+    [SerializeField, PropertyField]
+    private float myPropertyField = .75f;
+
+    [HorizontalLine(2, SuperColor.Green)]
+
+    [SerializeField, PropertyField("MyPropertyField")]
+    private int myInt = 3;
+
+
+    /**************************
+     *****   PROPERTIES   *****
+     *************************/
+
+    public float MyPropertyField
+    {
+        set
+        {
+            Debug.Log("Property Set");
+        }
+    }
+}
